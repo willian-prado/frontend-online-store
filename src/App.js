f7ca996eff11900bb53d7be514b123fb5e4379aa
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-12 16:57:10 -0300

    Refatorando `App`

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +19,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
