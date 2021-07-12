b8de3d6744b994995ba1dccba61d2158241f7f63
Marcela Silva <marcela.ops@hotmail.br>
2021-07-12 15:19:34 -0300

    requisito 1 requerimento API arquivo api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
   // Implemente aqui
+  const fetchURL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(fetchURL);
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(fetchURL);
+  return response.json();
 }
