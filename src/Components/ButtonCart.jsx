12e60d45fb57fa3bb64787acedb92f6fffdede24
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-07-12 18:32:23 -0300

    Requisito 3 - com icones.

---
+++ src/Components/ButtonCart.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { RiShoppingCartLine } from 'react-icons/ri';
+
+class ButtonCart extends Component {
+  render() {
+    return (
+      <div>
+        <Link
+          to="shopping-cart"
+          data-testid="shopping-cart-button"
+        >
+          <RiShoppingCartLine />
+
+        </Link>
+      </div>
+
+    );
+  }
+}
+
+export default ButtonCart;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ButtonCart from '../Components/ButtonCart';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <ButtonCart />
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
