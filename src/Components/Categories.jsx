bc311af84b0136ecb9adfad507da2673d039fafd
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-07-12 19:57:52 -0300

    Requisito 4 refatorado

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import * as api from '../services/api';
+
+class Categories extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+        fetchCategories = async () => {
+          const { getCategories } = api;
+          console.log(api);
+          const requestReturn = await getCategories();
+          console.log(requestReturn);
+          this.setState({
+            categories: [...requestReturn],
+          });
+        }
+
+        render() {
+          const { categories } = this.state;
+          return (
+            <ol>
+              {categories.map(({ id, name }) => (
+                <li
+                  key={ id }
+                  category={ name }
+                  data-testid="category"
+                >
+                  {name}
+                </li>))}
+            </ol>
+          );
+        }
+}
+
+export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,21 @@
 import React, { Component } from 'react';
 import ButtonCart from '../Components/ButtonCart';
 import * as api from '../services/api';
+import Categories from '../Components/Categories';
 
 class Home extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      categories: [],
-    };
-  }
-
-  componentDidMount() {
-    this.fetchCategories();
-  }
-
-  fetchCategories = async () => {
-    const { getCategories } = api;
-    console.log(api);
-    const requestReturn = await getCategories();
-    console.log(requestReturn);
-    this.setState({
-      categories: [...requestReturn],
-    });
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
-    const { categories } = this.state;
     return (
       <div>
         <ButtonCart />
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
-        <ol>
-          {categories.map(({ id, name }) => (
-            <li
-              key={ id }
-              category={ name }
-              data-testid="category"
-            >
-              {name}
-            </li>))}
-        </ol>
+        <Categories />
       </div>
     );
   }
