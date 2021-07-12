5d806c349e8d456984a3884cbb0a2f9e364fd4e1
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-12 17:10:27 -0300

    Criando page `Home`

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Home from './pages/Home';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={} />
+          <Route path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
     );
