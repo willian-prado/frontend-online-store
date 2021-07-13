79d1d489ff3c48d4ef8b3f7daf731e4cdc06b19c
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-13 15:55:43 -0300

    Criando a page `ProductDetails`

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProductDetails extends Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="product-detail-name">Name</p>
+      </div>
+    );
+  }
+}
