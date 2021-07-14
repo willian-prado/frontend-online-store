89fb5da425845478ecbd08f02b39173b41fa8703
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-14 10:54:48 -0300

    Adiciona `Link` no `ProductCard`

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
     const { product } = this.props;
     const { title, thumbnail, price } = product;
     return (
-      <div data-testid="product">
-        <div>
-          { title }
+      <Link to="/product-details" data-testid="product-detail-link">
+        <div data-testid="product">
+          <div>
+            { title }
+          </div>
+          <div>
+            <img src={ thumbnail } alt={ `${title}` } />
+          </div>
+          <div>
+            { price }
+          </div>
         </div>
-        <div>
-          <img src={ thumbnail } alt={ `${title}` } />
-        </div>
-        <div>
-          { price }
-        </div>
-      </div>
+      </Link>
     );
   }
 }
