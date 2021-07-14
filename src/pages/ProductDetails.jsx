60fa0f2ef890a173a0077a9a2495218e008310c2
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-14 13:47:19 -0300

    Adiciona imagem do produto e botões para trocar de imagem

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineCaretLeft, AiOutlineCaretRight } from 'react-icons/ai';
 
 export default class ProductDetails extends Component {
   constructor() {
@@ -9,7 +9,12 @@ export default class ProductDetails extends Component {
 
     this.state = {
       product: {},
+      pictures: [],
+      picIndex: 0,
     };
+
+    this.decreasePicIndex = this.decreasePicIndex.bind(this);
+    this.increasePicIndex = this.increasePicIndex.bind(this);
   }
 
   componentDidMount() {
@@ -23,15 +28,43 @@ export default class ProductDetails extends Component {
     const result = await response.json();
     this.setState({
       product: result,
+      pictures: result.pictures,
+    });
+  }
+
+  decreasePicIndex() {
+    this.setState((prevState) => {
+      const { picIndex, pictures } = prevState;
+      if (picIndex === 0) return { picIndex: (pictures.length - 1) };
+      return { picIndex: picIndex - 1 };
+    });
+  }
+
+  increasePicIndex() {
+    this.setState((prevState) => {
+      const { picIndex, pictures } = prevState;
+      if (picIndex === (pictures.length - 1)) {
+        return { picIndex: 0 };
+      }
+      return { picIndex: picIndex + 1 };
     });
   }
 
   render() {
-    const { product: { title } } = this.state;
+    const { product: { title }, pictures, picIndex } = this.state;
     return (
       <div>
         <Link to="/"><AiOutlineHome /></Link>
         <h3 data-testid="product-detail-name">{ title }</h3>
+        {pictures.length && <img src={ pictures[picIndex].url } alt={ title } />}
+        <div>
+          <button type="button" onClick={ this.decreasePicIndex }>
+            <AiOutlineCaretLeft />
+          </button>
+          <button type="button" onClick={ this.increasePicIndex }>
+            <AiOutlineCaretRight />
+          </button>
+        </div>
       </div>
     );
   }
