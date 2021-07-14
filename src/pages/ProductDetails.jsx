a98db8cee049505bc63706856d92d86e61b070fe
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-14 12:06:02 -0300

    Adiciona nome do produto em `ProductDetails`

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 export default class ProductDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      product: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  async getProduct() {
+    const { match: { params: { id } }} = this.props;
+
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const result = await response.json();
+    this.setState({
+      product: result,
+    });
+  }
+
   render() {
+    const { product: { title } } = this.state;
     return (
       <div>
-        <p data-testid="product-detail-name">Name</p>
+        <p data-testid="product-detail-name">{ title }</p>
       </div>
     );
   }
